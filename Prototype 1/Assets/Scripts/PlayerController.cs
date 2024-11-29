17cd0c0c85bf22acc905e17e07946b547563bd90
AndresG <53833302+Pi-face@users.noreply.github.com>
2024-11-29 10:47:34 -0600

    fix: removed un-needed variables

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 20;
 
-    // Update is called once per frame
     void Update()
     {
-       // we'll move the vehivle forward. 
-       transform.Translate(0,0,1);
-       
+       // we'll move the vehicle forward. 
+       transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+
     }
 }
