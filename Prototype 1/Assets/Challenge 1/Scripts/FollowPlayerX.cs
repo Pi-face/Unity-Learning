37a113aea9930f9eeff38a775b28950d0a975a22
AndresG <53833302+Pi-face@users.noreply.github.com>
2024-12-01 17:32:10 -0600

    bug; object going backwards and too fast

--- Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,20 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    private Vector3 offset = new Vector3(20, 0, 0);
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
     }
--- Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
@@ -8,22 +6,15 @@ public class PlayerControllerX : MonoBehaviour
     public float rotationSpeed;
     public float verticalInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * verticalInput * Time.deltaTime);
     }
 }
