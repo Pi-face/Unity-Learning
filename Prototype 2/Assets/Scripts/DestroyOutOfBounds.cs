8047640657aac6c4baf7f102d22fae57d4bdb054
Pi-face <guzdev52@gmail.com>
2024-12-17 17:49:51 -0600

    created animal prefabs and attatched a script

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class Destroy : MonoBehaviour
 {
     private float topBound = 30f;
+    private float lowerBound = -10f;
 
     void Update()
     {
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        }   
+        }   else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
