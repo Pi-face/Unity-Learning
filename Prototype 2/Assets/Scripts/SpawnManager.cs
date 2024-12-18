56e44e17246f611a20b4a1cfa930024c0ede02a2
Pi-face <guzdev52@gmail.com>
2024-12-17 22:02:16 -0600

    feat: made spawn zone a set distance

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex; 
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    
+    
     
     void Update()
-    {
+    {   
+        
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
